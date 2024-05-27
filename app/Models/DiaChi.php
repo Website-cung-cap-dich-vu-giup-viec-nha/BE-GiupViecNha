39abe60436f5d039ebb39de8543e797b381aa2bd
DangNgocThao <dangthao352002@gmail.com>
2024-05-27 22:25:51 +0700

    api DiaChi

--- app/Models/DiaChi.php
+++ app/Models/DiaChi.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class DiaChi extends Model
 {
     use HasFactory;
+    protected $table = "DiaChi";
+    protected $primaryKey = 'idDiaChi';
+    protected $fillable = [
+        "idDiaChi",
+        "Duong",
+        "Phuong",
+        "idNguoiDung",
+        "MacDinh"
+    ];
+    public $timestamps = false;
 }
--- app/Models/district.php
+++ app/Models/district.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class district extends Model
 {
     use HasFactory;
+    protected $table = "district";
 }
